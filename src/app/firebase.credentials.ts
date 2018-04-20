71f317f565c162abbb8e268b6afa63337936cb0e
lynsamson <36421496+lynsamson@users.noreply.github.com>
2018-04-20 10:45:21 +0400

    Add files via upload

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+    apiKey: "AIzaSyCBl3dvJIPTwrH7JxuN70eu3p9i30DcXwM",
+    authDomain: "send-a-cake.firebaseapp.com",
+    databaseURL: "https://send-a-cake.firebaseio.com",
+    projectId: "send-a-cake",
+    storageBucket: "send-a-cake.appspot.com",
+    messagingSenderId: "117422954756"
+};
